f9d7e3567157db2ea86f4a331f93efae796463ad
Mehemmed476 <mehemmedxelilzade11@gmail.com>
2025-11-28 15:20:32 +0400

    some changes

---
+++ Domain/Entities/FAQ.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities.Common;
+
+namespace Domain.Entities;
+
+public class FAQ : AuditableEntity
+{
+    public string Question { get; set; } = "";
+    public string Answer { get; set; } = "";
+    public int DisplayOrder { get; set; } = 0;
+    public bool IsActive { get; set; } = true;
+}
